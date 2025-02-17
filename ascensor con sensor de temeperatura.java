99d81da27dd04852db3b9eb3736898f7a44c8dfc
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-02-17 08:46:13 -0500

    Create ascensor con sensor de temeperatura.java

---
+++ ascensor con sensor de temeperatura.java
@@ -0,0 +1,31 @@
+package ascensor_con_sensor_de_temperatura;
+
+import java.util.Scanner;
+
+
+public class Ascensor_con_sensor_de_temperatura {
+
+    
+    public static void sensor(String[]) {
+       public static temperatura {
+        String numero_pisos="21";
+        Scanner TomarDatos = new Scanner(System.in);
+        System.out.print("por favor,ingrese su peso:");
+        System.out.print("ingrese el piso que desea ir");
+        int numero = TomarDatos.nextInt();
+        TomarDatos.close();
+        Scanner ProcesarDatos = new Scanner(System.in);
+        int numero = ProcesarDatos.nextInt();
+        ProcesarDatos.close();
+}
+        int temp=0
+         if(temp<=21){
+            System.out.println("la temperatura es valido el ascensor se puede mover");
+        }
+        else{
+            System.out.println("lo siento, el ascensor no se puede mover por temas de temepratura");
+        }
+        return null;
+    }
+    
+}
