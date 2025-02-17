1ebb22161dfb9284d822cc46d13aace3d14a74d2
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-02-17 08:47:37 -0500

    Create temperatura_ambiente.java

---
+++ temperatura_ambiente.java
@@ -0,0 +1,23 @@
+package temperatura_ambiente;
+
+import java.util.Scanner;
+
+
+public class Temperatura_ambiente {
+
+    
+    public static void main(String[] args) {
+        Scanner TomarDatos = new Scanner(System.in);
+        System.out.print("por favor, ingrese la temperatura ambiente");
+        int temperatura = TomarDatos.nextInt();
+        TomarDatos.close();
+        if(temperatura°)int °int C = 21;
+{
+           System.out.println("la temepratura es valida el ascensor esta bajando");
+    }
+        else{
+                System.out.println("la temperatura escede el limite adecuado el ascensor no se movera");
+                }
+    }
+    
+}
