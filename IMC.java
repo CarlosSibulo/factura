889b3b66f1a3dfb777977fb99c07275580c67518
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-02-17 09:47:07 -0500

    Create IMC.java

---
+++ IMC.java
@@ -0,0 +1,24 @@
+package imc;
+
+
+public class IMC {
+
+    
+    public static void main(String[] args) {
+        Scanner OpcionValor = new Scanner(System.in);
+        System.out.print("por favor ingrese su peso,ingrese su peso.Calcular/");
+        int pesoopcion= pesoValor.nextInt();
+        switch (peso){
+            case 1:
+                bajo peso: menor<18.5
+            case 2:
+                peso normal:menor<24.9
+            case 3:
+                sobrepeso:menor<29.9
+            case 4:
+                obesidad:menor<29.90
+              
+        }
+    }
+    
+}
