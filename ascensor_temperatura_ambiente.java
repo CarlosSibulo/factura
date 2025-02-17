4c9065b0ad1534efc6d8651a00afefa8bd3284b7
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-02-17 08:50:31 -0500

    Create ascensor_temperatura_ambiente.java

---
+++ ascensor_temperatura_ambiente.java
@@ -0,0 +1,23 @@
+package ascensor_sensor_teperatura_ambiente;
+
+import java.util.Scanner;
+
+
+public class Ascensor_sensor_teperatura_ambiente {
+
+    
+    public static void main(String[] args) {
+        Scanner TomarDatos = new Scanner(System.in);
+        System.out.print("por favor, ingrese la temperatura ambiente");
+        int temperatura = TomarDatos.nextInt();
+        TomarDatos.close();
+        if(temperatura°)int C21 = 0;
+{
+          System.out.println("la temperatura esta dentro de los parametros el ascensor se movera");
+    }
+        else{
+                System.out.println("la temperatura escede limite adecuado el ascensor no se movera");
+                }
+    }
+    
+}
