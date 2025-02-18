e297d3fcaee2db6fad6dd521d287dc2a0777ea80
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-02-18 08:31:42 -0500

    Create cajero_automatico.java

---
+++ cajero_automatico.java
@@ -0,0 +1,17 @@
+public class cajero_automatico {
+    public static void main(String[]args){
+        int opcion = 0;
+        Scanner dato_opcion= new Scanner(System.in);
+        System.out.print("por favor,ingrese su tarjeta");
+        opcion= dato_opcion.nextInt();
+        System.out.print("por favor,ingrese el numero de monto qequerido");
+        Scanner dato_opcion= new Scanner(System.in);
+        while (monto=saldo){
+            System.out.println("el monto es mayor al saldo requerido");
+            System.out.println("el monto es menor al saldo requerido");
+            System.out.println("error fondos insuficientes");
+            opcion= dato_opcion.nextInt();
+        }
+    }
+    
+}
