6aed5c95f3dfa95c39137fd217bc8ad120184e6f
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-03-04 09:14:23 -0500

    Create sensor_temperatura.java

---
+++ sensor_temperatura.java
@@ -0,0 +1,21 @@
+package sensor_temperatura;
+import java.util.Scanner;
+
+public class sensor_temperatura {
+public static temperatura {
+    Scanner TomarDatos = new Scanner(System.in);
+    System.out.print("por favor,ingrese la temperatura");
+    System.out.print("lo siento,latemperatura no es adecuada");
+    int numero = TomarDatos.nextInt();
+    procesarDatos.close();
+}
+int temp = 0
+        if(temp<=31){
+    System.out.println("la temperatura es valida");
+}
+        else{
+    System.out.println("lo siento,la temperatura no esla adecuada");
+}
+        return null
+    
+}
