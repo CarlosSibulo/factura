7e588f6536291b43fc05e1c0360a14f825fc436a
CarlosSibulo <carlosmauricio0212@gmail.com>
2025-03-07 07:30:53 -0500

    Update IMC.java

--- IMC.java
+++ IMC.java
@@ -1,24 +1,37 @@
-package imc;
+package Ejercicios.java;
+
+import java.util.Scanner;
 
 
 public class IMC {
-
-    
-    public static void main(String[] args) {
-        Scanner OpcionValor = new Scanner(System.in);
-        System.out.print("por favor ingrese su peso,ingrese su peso.Calcular/");
-        int pesoopcion= pesoValor.nextInt();
-        switch (peso){
-            case 1:
-                bajo peso: menor<18.5
-            case 2:
-                peso normal:menor<24.9
-            case 3:
-                sobrepeso:menor<29.9
-            case 4:
-                obesidad:menor<29.90
-              
-        }
+    public static void main (String[] args){
+     IMC();
     }
-    
+    public static void IMC (String[] args){
+    Scanner TomarDatos = new Scanner(System.in);
+    System.out.print("Ingrese su peso en kg: ");
+    int peso = Scanner.nextInt();
+    System.out.print("Ingrese su altura en metros: ");
+    int altura = Scanner.nextInt(); 
+    int imc = TomarDatos.nextInt();
+    Scanner ProcesarDatos = new Scanner(System.in);
+    int imc = ProcesarDatos.nextInt();
+    switch (imc == Peso/Altura^[2]) {
+            case 'Menor a 18.5':
+                System.out.println("Bajo Peso");
+                break;
+            case 'Menor a 24.9':
+                System.out.println("Peso Normal");
+                break;
+            case 'Menor a 29.9':
+                System.out.println("SobrePeso");
+                break;
+             case 'Mayor a 29.90':
+                System.out.println("Obecidad");
+                break;
+            default:
+                System.out.println("Error");
+                return;
+      }
+    }
 }
